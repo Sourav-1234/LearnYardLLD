418a11a076bc0fedaa533f32544fe6669c26a117
intern2_crosscut <sourav024saha@gmail.com>
2024-05-25 16:55:42 +0530

    Order Management System

--- 12-May-2024/Python/order_management_system.py
+++ 12-May-2024/Python/order_management_system.py
@@ -1,5 +1,7 @@
 from transitions import Machine
 import queue
+import threading
+import time 
 
 class Order:
     states= ['placed','in_progress','in_delivery','canceled','completed']
@@ -10,12 +12,38 @@ class Order:
 
 
         self.machine.add_transition(trigger='progress',source='placing',dest='in_progress')
-        
+
         self.machine.add_transition(trigger='deliver',source='in_progress',dest='in_delivery')
-        
+
         self.machine.add_transition(trigger='cancel',source='*',dest='canceled')
 
         self.machine.add_transition(trigger='complete',source='in_delivery' ,dest='completed')
+
+
+class Producer(threading.Thread):
+    def __init__(self,order_system):
+        threading.Thread.__init__(self)
+        self.order_system = order_system
+
+
+    def run(self):
+        for i in range(12):
+            self.order_system.create_order(f"Order {i}")
+            time.sleep(1)
+
+
+class Consumer(threading.Thread):
+    def __init__(self,order_system):
+        threading.Thread.__init__(self)
+        self.order_system=order_system
+
+
+    def run(self):
+        while True:
+            self.order_system.process_events()
+            print("Consumed Orders")
+            time.sleep(1)
+
 
 
 
@@ -42,7 +70,7 @@ class OrderSystem:
             self.event_queue.put((self.orders[name],'deliver'))
             print("Delivered Order")
 
-    
+
     def complete_order(self,name):
         if name in self.orders and self.orders[name].state=='in_delivery':
             self.event_queue.put((self.orders[name],'complete'))
@@ -55,12 +83,20 @@ class OrderSystem:
 
 
 
-system=OrderSystem()
-system.create_order('Order1')
-system.process_events()
-system.deliver_order('Order1')
-system.process_events()
-system.complete_order('Order1')
-system.process_events()
+order_system=OrderSystem()
+producer=Producer(order_system)
+consumer=Consumer(order_system)
+producer.start()
+consumer.start()
+producer.join()
+consumer.join()
+
+
+# system.create_order('Order1')
+# system.process_events()
+# system.deliver_order('Order1')
+# system.process_events()
+# system.complete_order('Order1')
+# system.process_events()
 
 
